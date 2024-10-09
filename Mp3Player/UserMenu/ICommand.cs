dc27fe7aea7e831737fdddd095301b251a1aee7c
Dar1en9 <alinfyodorowa25@gmail.com>
2024-10-09 18:22:34 +0300

    renamed

--- Mp3Player/UserMenu/ICommand.cs
+++ Mp3Player/UserMenu/ICommand.cs
@@ -1,6 +1,6 @@
 ﻿namespace Mp3Player.UserMenu;
 
-public interface AbstractCommand<T, in T1>
+public interface ICommand<T, in T1>
 {
     public string? Description { get; }
     public abstract Task<T> Execute(T1? arg = default);
