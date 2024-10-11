9f9c07b9fe9341fb2db59826631df1296faae1f0
Dar1en9 <alinfyodorowa25@gmail.com>
2024-10-11 14:01:00 +0300

    creating admin command for adding track

---
+++ Mp3Player/MenuCommands/AdminMenu/AddTrackCommand.cs
@@ -0,0 +1,27 @@
+﻿using Mp3Player.DataBase;
+
+namespace Mp3Player.MenuCommands.AdminMenu;
+
+public class AddTrackCommand: ICommand<bool, string>
+{
+    private readonly Track _track;
+    private readonly IDataBaseWriter _dataBaseWriter;
+    public string? Description { get; } = "Добавить трек";
+
+    public AddTrackCommand(Track track, IDataBaseWriter dataBaseWriter)
+    {
+        _track = track;
+        _dataBaseWriter = dataBaseWriter;
+    }
+    
+    Task IUniCommand<string>.Execute(string? arg)
+    {
+        return Execute(arg);
+    }
+    
+    public async Task<bool> Execute(string? arg = default)
+    {
+        await _dataBaseWriter.WriteTrack(_track);
+        return true;
+    }
+}
