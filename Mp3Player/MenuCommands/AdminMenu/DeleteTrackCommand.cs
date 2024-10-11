5e195ad617ef18810fd0ab7398bf1fce826cabb7
Dar1en9 <alinfyodorowa25@gmail.com>
2024-10-11 14:38:11 +0300

    creating admin command for deleting tracks

---
+++ Mp3Player/MenuCommands/AdminMenu/DeleteTrackCommand.cs
@@ -0,0 +1,27 @@
+﻿using Mp3Player.DataBase;
+
+namespace Mp3Player.MenuCommands.AdminMenu;
+
+public class DeleteTrackCommand: ICommand<bool, string>
+{
+    private readonly int _trackId;
+    private readonly IDataBaseDeleter _dataBaseDeleter;
+    public string? Description { get; } = "Удалить трек";
+
+    public DeleteTrackCommand(int trackId, IDataBaseDeleter dataBaseDeleter)
+    {
+        _trackId = trackId;
+        _dataBaseDeleter = dataBaseDeleter;
+    }
+    
+    Task IUniCommand<string>.Execute(string? arg)
+    {
+        return Execute(arg);
+    }
+    
+    public async Task<bool> Execute(string? arg = default)
+    {
+        await _dataBaseDeleter.DeleteTrack(_trackId);
+        return true;
+    }
+}
