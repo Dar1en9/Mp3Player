5ea6d852c364e0e47350838e7379cf453c8e2701
Dar1en9 <alinfyodorowa25@gmail.com>
2024-10-23 20:10:15 +0300

    creating play command

---
+++ Mp3Player/MenuCommands/PlayerMenu/PlayCommand.cs
@@ -0,0 +1,42 @@
+﻿using Mp3Player.TrackCreator;
+using NetCoreAudio;
+
+namespace Mp3Player.MenuCommands.PlayerMenu;
+
+public class PlayCommand : ICommand<bool, string>
+{
+    private readonly Track _track;
+    private readonly Player _player;
+    public string? Description { get; } = "Воспроизведение";
+
+    public PlayCommand(Track track, Player player)
+    {
+        _track = track;
+        _player = player;
+    }
+    
+    Task IUniCommand<string>.Execute(string? arg)
+    {
+        return Execute(arg);
+    }
+    
+    public async Task<bool> Execute(string? arg = default)
+    {
+        _player.PlaybackFinished -= OnPlaybackFinished; // Отписка от события
+        _player.PlaybackFinished += OnPlaybackFinished; // Подписка на событие
+        try
+        {
+            await _player.Play(_track.audioPath);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        return true;
+    }
+
+    private static void OnPlaybackFinished(object? sender, EventArgs e)
+    {
+        Console.WriteLine("Воспроизведение завершено. Вы можете выбрать другой трек или вернуться в меню");
+    }
+}
