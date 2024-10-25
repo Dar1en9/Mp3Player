487d55e201c6444b55bd1c6f90af3c9df8eb8f46
Dar1en9 <alinfyodorowa25@gmail.com>
2024-10-25 17:22:08 +0300

    adding history saver while searching for track

--- Mp3Player/Menu/Commands/UserCommands/FindTracksCommand.cs
+++ Mp3Player/Menu/Commands/UserCommands/FindTracksCommand.cs
@@ -1,4 +1,5 @@
 ﻿using Mp3Player.DataBase;
+using Mp3Player.History;
 using Mp3Player.TrackCreator;
 
 namespace Mp3Player.Menu.Commands.UserCommands;
@@ -7,12 +8,14 @@ public class FindTracksCommand: ICommand<List<Track>, string>
 {
     private readonly string _professor;
     private readonly IDataBaseReader _dataBaseReader;
+    private readonly IHistoryManager _historyManager;
     public string Description { get; } = "Найти трек по преподавателю";
     
-    public FindTracksCommand(string professor, IDataBaseReader dataBaseReader)
+    public FindTracksCommand(string professor, IDataBaseReader dataBaseReader, IHistoryManager historyManager)
     {
         _professor = professor;
         _dataBaseReader = dataBaseReader;
+        _historyManager = historyManager;
     }
     
     Task IUniCommand<string>.Execute(string? arg)
@@ -22,6 +25,7 @@ public class FindTracksCommand: ICommand<List<Track>, string>
 
     public async Task<List<Track>> Execute(string? arg = default)
     {
+        await _historyManager.WriteHistory(_professor);   
         return await _dataBaseReader.GetProfessorTracks(_professor);
     }
 }
