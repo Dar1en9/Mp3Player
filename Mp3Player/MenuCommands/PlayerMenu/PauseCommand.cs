b442b9d5d21e5a1e89fb525eec95409eaa061e03
Dar1en9 <alinfyodorowa25@gmail.com>
2024-10-25 15:59:58 +0300

    creating track pause command

---
+++ Mp3Player/MenuCommands/PlayerMenu/PauseCommand.cs
@@ -0,0 +1,36 @@
+﻿using Mp3Player.TrackCreator;
+using NetCoreAudio;
+
+namespace Mp3Player.MenuCommands.PlayerMenu;
+
+public class PauseCommand : ICommand<bool, string>
+{
+    private readonly Track _track;
+    private readonly Player _player;
+    public string? Description { get; } = "Пауза";
+
+    public PauseCommand(Track track, Player player)
+    {
+        _track = track;
+        _player = player;
+    }
+    
+    Task IUniCommand<string>.Execute(string? arg)
+    {
+        return Execute(arg);
+    }
+    
+    public async Task<bool> Execute(string? arg = default)
+    {
+        try
+        {
+            await _player.Pause();
+            Console.WriteLine("Трек на паузе");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        return true;
+    }
+}
