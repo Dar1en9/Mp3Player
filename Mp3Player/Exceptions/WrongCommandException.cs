735d66ceeffecb31374262ec305674c72b8afeac
Dar1en9 <alinfyodorowa25@gmail.com>
2024-10-25 16:58:48 +0300

    creating command exception

---
+++ Mp3Player/Exceptions/WrongCommandException.cs
@@ -0,0 +1,9 @@
+﻿namespace Mp3Player.Exceptions;
+
+public class WrongCommandException() : Exception("Неверная команда. Введите 0 для вывода списка доступных команд")
+{
+    public override string ToString()
+    {
+        return "Неверная команда";
+    }
+}
