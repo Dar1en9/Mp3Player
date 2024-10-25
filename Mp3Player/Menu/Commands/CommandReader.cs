dc36b4bb3e071ff8118591148b1624c0ded0bce9
Dar1en9 <alinfyodorowa25@gmail.com>
2024-10-25 17:06:53 +0300

    creating command reader

---
+++ Mp3Player/Menu/Commands/CommandReader.cs
@@ -0,0 +1,23 @@
+﻿using Mp3Player.Exceptions;
+
+namespace Mp3Player.Menu.Commands;
+
+public class CommandReader
+{
+    public async Task<int> getCommand()
+    {
+        while (true)
+        {
+            var command = await Console.In.ReadLineAsync();
+            if (int.TryParse(command, out var key)) return key;
+            try
+            {
+                throw new WrongCommandException();
+            }
+            catch (WrongCommandException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
