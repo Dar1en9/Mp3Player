20a45167353a891486c563a1fe2a04cd23088d40
Dar1en9 <alinfyodorowa25@gmail.com>
2024-10-26 16:49:03 +0300

    creating main menu runner for user and meeting a problem with parameters while creating Dictionary

---
+++ Mp3Player/MainMenuUserRunner.cs
@@ -0,0 +1,29 @@
+﻿using Mp3Player.Menu.Commands;
+using Mp3Player.Menu.Commands.UserCommands;
+
+namespace Mp3Player;
+
+public class MainMenuUserRunner : IRunner
+{
+    private readonly CommandReader _reader = new CommandReader();
+   // public MainMenuUserRunner(Dictionary<int, IUniCommand> commands) => _commands = commands;
+    private static readonly Dictionary<int, IUniCommand> _commands = new()
+    {
+        {1, new FindTracksCommand()} //а как какать?
+    };
+    
+    public Task ExecuteCommand()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IUniCommand> CommandHandler()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Run()
+    {
+        throw new NotImplementedException();
+    }
+}
