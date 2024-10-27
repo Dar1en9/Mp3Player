9e580ec25e64d7546526f106415bc9d3dce675ec
Dar1en9 <alinfyodorowa25@gmail.com>
2024-10-27 23:36:41 +0300

    creating main menu page, but it needs fixes for not executing commands but pushing buttons with further navigation

---
+++ Mp3Player/Menu/UserMenu/MainMenu.cs
@@ -0,0 +1,55 @@
+﻿using Mp3Player.Exceptions;
+using Mp3Player.Menu.Commands.UserCommands;
+using Mp3Player.InputReaders;
+using Mp3Player.Menu.Commands;
+
+
+namespace Mp3Player.Menu.UserMenu;
+
+public class MainMenu : IMenu
+{
+    private readonly ICommandReader _commandReader;
+    private readonly Dictionary<int, IUniCommand> _commands;
+    private readonly ShowMenuCommand _showMenu;
+    
+    public MainMenu(ICommandReader commandReader, Dictionary<int, IUniCommand> commands)
+    {
+        _commandReader = commandReader;
+        _commands = commands;
+        _showMenu = new ShowMenuCommand(commands);
+    }
+    
+    public async Task<IMenu> Run()
+    {
+        Console.WriteLine("##Главное меню");
+        var showMenuTask = ExecuteCommand(_showMenu);
+        var newCommandTask = ExecuteCommand(await CommandHandler());
+        await Task.WhenAll(showMenuTask, newCommandTask);
+        return this;
+    }
+    
+    public async Task ExecuteCommand(IUniCommand command)
+    {
+        var execute = command.Execute();
+        //залогировать
+        await Task.WhenAll(execute); //добавить помимо execute логи
+    }
+
+    public async Task<IUniCommand> CommandHandler()
+    {
+        while (true)
+        {
+            var key = await _commandReader.GetInput();
+            if (_commands.TryGetValue(key, out var command)) return command;
+            try
+            {
+                throw new WrongCommandException();
+            }
+            catch (WrongCommandException e)
+            {
+                //сделать логи
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
