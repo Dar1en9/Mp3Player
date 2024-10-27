2a2284fa34026b62cbcd1cd6cb27fbdf381156c0
Dar1en9 <alinfyodorowa25@gmail.com>
2024-10-27 23:38:35 +0300

    making track creator for adding new tracks by admin

---
+++ Mp3Player/TrackHandler/TrackCreator.cs
@@ -0,0 +1,22 @@
+﻿using Mp3Player.InputReaders;
+
+namespace Mp3Player.TrackHandler;
+
+public class TrackCreator
+{
+    private readonly IProfessorReader _professorReader;
+    private readonly IAudioPathReader _audioPathReader;
+
+    public TrackCreator(IProfessorReader professorReader, IAudioPathReader audioPathReader)
+    {
+        _professorReader = professorReader;
+        _audioPathReader = audioPathReader;
+    }
+
+    public async Task<Track> NewTrack()
+    {
+        var name = await _professorReader.GetInput();
+        var audioPath = await _audioPathReader.GetInput();
+        return new Track(name, new TrackId(), audioPath);
+    }
+}
