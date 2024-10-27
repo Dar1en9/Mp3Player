c0c227e1fde80a61515d6bda95778be3d86648f5
Dar1en9 <alinfyodorowa25@gmail.com>
2024-10-27 23:38:05 +0300

    creating a button for getting tracks but needs fixes for using in main menu

---
+++ Mp3Player/Menu/Buttons/ShowTracksButton.cs
@@ -0,0 +1,22 @@
+﻿using Mp3Player.Menu.Commands;
+using Mp3Player.Menu.Commands.UserCommands;
+
+namespace Mp3Player.Menu.Buttons;
+
+public class ShowTracksButton : IButton //только наверное лучше на каждый поиск отдельно и потом в mainmenu мы
+//делаем не executeCommand() а там OnClick() вызываем
+{
+    private readonly IMenuNavigator _menuNavigator;
+    private readonly ITrackListCommand _command;
+
+    public ShowTracksButton(IMenuNavigator menuNavigator, ITrackListCommand command)
+    {
+        _menuNavigator = menuNavigator;
+        _command = command;
+    }
+    
+    public async Task OnClick()
+    {
+        _menuNavigator.NavigateToTrackList(await _command.Execute());
+    }
+}
