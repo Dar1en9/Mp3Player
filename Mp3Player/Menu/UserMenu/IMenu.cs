81d59a83044cbb144265406d6541385c9b29ba29
Dar1en9 <alinfyodorowa25@gmail.com>
2024-10-27 23:34:40 +0300

    creating interface for menu pages

---
+++ Mp3Player/Menu/UserMenu/IMenu.cs
@@ -0,0 +1,10 @@
+﻿using Mp3Player.Menu.Commands;
+
+namespace Mp3Player.Menu.UserMenu;
+
+public interface IMenu
+{
+    Task<IMenu> Run();
+    Task ExecuteCommand(IUniCommand command);
+    Task<IUniCommand> CommandHandler();
+}
