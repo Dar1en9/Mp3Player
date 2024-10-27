54784ab984a204d6db45c1a848556218651aacd4
Dar1en9 <alinfyodorowa25@gmail.com>
2024-10-27 18:27:18 +0300

    creating reader of absolute path

---
+++ Mp3Player/InputReaders/PathReader.cs
@@ -0,0 +1,25 @@
+﻿using Mp3Player.Exceptions;
+using System.IO;
+
+namespace Mp3Player.InputReaders;
+
+public class PathReader : IReader<string>
+{
+    public async Task<string> GetInput()
+    {
+        while (true)
+        {
+            await Console.Out.WriteLineAsync("Введите полный путь файла:");
+            var path = await Console.In.ReadLineAsync();
+            if (path != null && Path.IsPathFullyQualified(path)) return path;
+            try
+            {
+                throw new WrongInputException();
+            }
+            catch (WrongCommandException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
