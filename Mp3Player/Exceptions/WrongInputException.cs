866e34cce46b64316b7cc6310035e7603c0d241f
Dar1en9 <alinfyodorowa25@gmail.com>
2024-10-27 18:09:47 +0300

    creating exception for incorrect inputs

---
+++ Mp3Player/Exceptions/WrongInputException.cs
@@ -0,0 +1,10 @@
+﻿namespace Mp3Player.Exceptions;
+
+public class WrongInputException() : Exception("Данные введены некорректно. Попробуйте снова")
+{
+    
+    public override string ToString()
+    {
+        return "Некорректный ввод данных";
+    }
+}
