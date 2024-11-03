78cd16b76fde4c44c20281c94fcd0e02fb2c26ce
Dar1en9 <alinfyodorowa25@gmail.com>
2024-11-04 00:09:38 +0300

    adding resume command

---
+++ Mp3Player/Menu/Commands/PlayerCommands/ResumeCommand.cs
@@ -0,0 +1,33 @@
+﻿using NetCoreAudio;
+
+namespace Mp3Player.Menu.Commands.PlayerCommands;
+
+public class ResumeCommand : ICommand<bool, string>
+    {
+    private readonly Player _player;
+    public string Description => "Возобновить";
+
+    public ResumeCommand(Player player)
+    {
+        _player = player;
+    }
+    
+    Task IUniCommand.Execute()
+    {
+        return Execute();
+    }
+    
+    public async Task<bool> Execute(string? arg = default)
+    {
+        try
+        {
+            await _player.Resume();
+            Console.WriteLine("Трек возобновлен");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        return true;
+    }
+}
