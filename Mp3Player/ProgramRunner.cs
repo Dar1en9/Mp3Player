527fee317edcbcfeb858d10805c9428362bc98bb
Dar1en9 <alinfyodorowa25@gmail.com>
2024-11-11 00:18:32 +0300

    adding an Admin menu run

--- Mp3Player/ProgramRunner.cs
+++ Mp3Player/ProgramRunner.cs
@@ -4,7 +4,7 @@ using Mp3Player.Menu;
 
 namespace Mp3Player;
 
-public class ProgramRunner: IProgramRunner
+public class ProgramRunner(string[] args): IProgramRunner
 {
     private readonly string _path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
         MyMusic), "MP3Player"); //папка MP3Player в папке MyMusic
@@ -14,6 +14,7 @@ public class ProgramRunner: IProgramRunner
         if (!Directory.Exists(_path)) Directory.CreateDirectory(_path);
         var storagePath = Path.Combine(_path, "Storage");
         if (!Directory.Exists(storagePath)) Directory.CreateDirectory(storagePath);
-        await new UserMenu(storagePath, _path).Run();
+        if (args.Contains("admin")) await new AdminMenu(storagePath).Run();
+        else await new UserMenu(storagePath, _path).Run();
     }
 }
