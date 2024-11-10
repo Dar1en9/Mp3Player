6870d3a9a6b1d94f4ed386e14c23f98fde6011a3
Dar1en9 <alinfyodorowa25@gmail.com>
2024-11-11 00:18:04 +0300

    creating a MissClick exception for using it when the input is empty

---
+++ Mp3Player/Exceptions/MissClickException.cs
@@ -0,0 +1,9 @@
+﻿namespace Mp3Player.Exceptions;
+
+public class MissClickException() : Exception("Возврат на прошлую страницу")
+{
+    public override string ToString()
+    {
+        return "Ошибочное нажатие";
+    }
+}
