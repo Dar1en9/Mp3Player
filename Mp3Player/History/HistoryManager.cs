b0c7751d12aa5c07602d1748cd20eb72f8ecb3d7
Dar1en9 <alinfyodorowa25@gmail.com>
2024-11-10 15:53:22 +0300

    reverting to getting a directory path from parameters and then creating a history file

--- Mp3Player/History/HistoryManager.cs
+++ Mp3Player/History/HistoryManager.cs
@@ -4,9 +4,9 @@ public class HistoryManager: IHistoryManager
 {
     private readonly string _fullPath;
 
-    public HistoryManager(string fullPath)
+    public HistoryManager(string path)
     {
-        _fullPath = fullPath;
+        _fullPath = Path.Combine(path, "history.txt");;
     }
     
     public async Task WriteHistory(string keyWord)
@@ -14,8 +14,9 @@ public class HistoryManager: IHistoryManager
         await File.WriteAllTextAsync(_fullPath, keyWord);
     }
     
-    public async Task<string> GetHistory()
+    public async Task<string> GetHistory() 
     {
+        if (!File.Exists(_fullPath)) throw new FileNotFoundException();
         return await File.ReadAllTextAsync(_fullPath);
     }
 }
