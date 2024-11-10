427731ff6706db720d51c10b14d19144eab598cd
Dar1en9 <alinfyodorowa25@gmail.com>
2024-11-11 00:15:43 +0300

    using an id reader instead of getting id from parameters and catching a MissClick exception

--- Mp3Player/Menu/Commands/AdminCommands/DeleteTrackCommand.cs
+++ Mp3Player/Menu/Commands/AdminCommands/DeleteTrackCommand.cs
@@ -1,16 +1,19 @@
 ﻿using Mp3Player.DataBase;
+using Mp3Player.Exceptions;
+using Mp3Player.InputReaders;
+using Mp3Player.TrackHandler;
 
 namespace Mp3Player.Menu.Commands.AdminCommands;
 
 public class DeleteTrackCommand: ICommand<bool, string>
 {
-    private readonly int _trackId;
+    private readonly IReader<string> _idReader;
     private readonly IDataBaseDeleter _dataBaseDeleter;
     public string? Description { get; } = "Удалить трек";
 
-    public DeleteTrackCommand(int trackId, IDataBaseDeleter dataBaseDeleter)
+    public DeleteTrackCommand(IReader<string> idReader, IDataBaseDeleter dataBaseDeleter)
     {
-        _trackId = trackId;
+        _idReader = idReader;
         _dataBaseDeleter = dataBaseDeleter;
     }
     
@@ -21,7 +24,17 @@ public class DeleteTrackCommand: ICommand<bool, string>
     
     public async Task<bool> Execute(string? arg = default)
     {
-        await _dataBaseDeleter.DeleteTrack(_trackId);
+        try
+        {
+            var trackId = await _idReader.GetInput();
+            await _dataBaseDeleter.DeleteTrack(trackId);
+        }
+        catch (MissClickException ex)
+        {
+            await Console.Out.WriteLineAsync(ex.Message);
+            return false;
+        }
+
         return true;
     }
 }
