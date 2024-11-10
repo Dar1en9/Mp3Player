5cf6d0a308abfbf7580d55f4fa9b7c5c8d98ca76
Dar1en9 <alinfyodorowa25@gmail.com>
2024-11-11 00:11:32 +0300

    creating a reader for track id to use in admin menu

---
+++ Mp3Player/InputReaders/IdReader.cs
@@ -0,0 +1,13 @@
+﻿using Mp3Player.Exceptions;
+
+namespace Mp3Player.InputReaders;
+
+public class IdReader: IReader<string>
+{
+    public async Task<string> GetInput()
+    {
+        await Console.Out.WriteLineAsync("Введите id трека для удаления:");
+        var id = await Console.In.ReadLineAsync();
+        return id ?? throw new MissClickException();
+    }
+}
