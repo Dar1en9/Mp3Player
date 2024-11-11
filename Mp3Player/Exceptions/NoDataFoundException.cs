3ca2475a03c1cfee6040d8db140d5342aebc2648
Dar1en9 <alinfyodorowa25@gmail.com>
2024-11-11 17:10:08 +0300

    creating an exception for situations when data wasn't found

---
+++ Mp3Player/Exceptions/NoDataFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Mp3Player.Exceptions;
+
+public class NoDataFoundException() : Exception("Данные не найдены")
+{
+    public override string ToString()
+    {
+        return "Нет данных";
+    }
+}
