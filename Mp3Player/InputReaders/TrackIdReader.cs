86f054e7b63337ff4227d8e58f531e2b902876d8
Dar1en9 <alinfyodorowa25@gmail.com>
2024-11-19 15:19:35 +0300

    renaming and adding logs

---
+++ Mp3Player/InputReaders/TrackIdReader.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+using Mp3Player.Exceptions;
+
+namespace Mp3Player.InputReaders;
+
+public class TrackIdReader(ILogger logger): IReader<string>
+{
+    public async Task<string> GetInput(CancellationToken cancellationToken = default)
+    {
+        logger.LogInformation("Запрос ввода идентификатора трека");
+        await Console.Out.WriteLineAsync("Введите id трека для удаления:");
+        var id = await Console.In.ReadLineAsync(cancellationToken);
+        logger.LogInformation("Администратор ввел идентификатор трека: {TrackId}", id);
+        if (!string.IsNullOrWhiteSpace(id)) return id;
+        logger.LogInformation("Идентификатор трека пустой или содержит только пробелы");
+        throw new MissClickException();
+    }
+}
