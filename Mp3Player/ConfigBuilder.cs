edb0236dde768ba5f42ffdee57af7dab340abdf3
Dar1en9 <alinfyodorowa25@gmail.com>
2025-01-01 21:07:27 +0300

    providing connection to db and creating query & execute func

---
+++ Mp3Player/ConfigBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Mp3Player;
+
+public sealed class ConfigBuilder
+{
+    private static readonly IConfigurationRoot Config = new ConfigurationBuilder() 
+        .AddJsonFile("appsettings.json")
+        .AddEnvironmentVariables()
+        .Build();
+    public static readonly ConfigBuilder AppConfigSettings = Config.GetRequiredSection("ConnectionStrings")
+        .Get<ConfigBuilder>()!;
+    public required string DefaultConnection { get; init; }
+}
---
+++ Mp3Player/DataBase/DbService.cs
@@ -0,0 +1,29 @@
+﻿namespace Mp3Player.DataBase;
+
+using System.Data;
+using Npgsql;
+using Dapper;
+
+public class DbService
+{
+    private readonly string _connectionString = ConfigBuilder.AppConfigSettings.DefaultConnection;
+    private async Task<IDbConnection> GetConnection()
+    {
+        var connection = new NpgsqlConnection(_connectionString);
+        await connection.OpenAsync();
+        return connection;
+    }
+
+    public async Task<IEnumerable<T>> QueryAsync<T>(string sql, DynamicParameters? parameters = null)
+    {
+        using var connection = await GetConnection();
+        return await connection.QueryAsync<T>(sql, parameters);
+    }
+
+    public async Task<int> ExecuteAsync(string sql, DynamicParameters? parameters = null)
+    {
+        using var connection = await GetConnection();
+        return await connection.ExecuteAsync(sql, parameters);
+    }
+}
+
