5215bdf74de94514c07857619ebbffe0e1b0f4a8
Dar1en9 <alinfyodorowa25@gmail.com>
2025-01-06 22:33:44 +0300

    creating interface for db service

---
+++ Mp3Player/DataBase/IDataBaseService.cs
@@ -0,0 +1,10 @@
+﻿using Dapper;
+
+namespace Mp3Player.DataBase;
+
+public interface IDataBaseService
+{
+    Task<IEnumerable<T>> QueryAsync<T>(string sql, DynamicParameters? parameters = null);
+    Task<int> ExecuteAsync(string sql, DynamicParameters? parameters = null);
+
+}
